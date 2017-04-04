fee88fe52b34499f8a7f3702cf4adeacbbeeaa74
mamihery <mamihery@MC-G-LP-C120-4.local>
2017-04-04 15:23:23 +0200

    create server button

---
+++ Assets/_scene/Scripts/popup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popup : MonoBehaviour {
+
+	public bool showPopup = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
